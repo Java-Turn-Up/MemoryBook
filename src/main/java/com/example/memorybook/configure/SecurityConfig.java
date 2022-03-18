e2e424bab1f654250061b617220ec51e102c2c65
yimo22 <yimo22@naver.com>
2022-03-18 18:05:36 +0900

    Feat/Bug : Login with in memory
    login with user(password) or admin(admin) successful.
    but not in DB.
    
    if it is successful with login, then go to swagger-ui/index.html.

--- src/main/java/com/example/memorybook/configure/SecurityConfig.java
+++ src/main/java/com/example/memorybook/configure/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.example.memorybook.configure;
 
+import com.example.memorybook.model.entity.Member;
+import com.example.memorybook.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,50 +14,65 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    @SuppressWarnings("unused")
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
+    @Autowired
+    private MemberRepository memberRepository;
+    /*
+     * Spring Security 가 사용자를 인증하는 방법이 담긴 객체
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("user").password(passwordEncoder().encode( "password")).roles("USER").and()
-                .withUser("admin").password(passwordEncoder().encode("password")).roles("USER", "ADMIN");
+                    .withUser("user").password(passwordEncoder().encode( "password")).roles("USER")
+                .and()
+                    .withUser("admin").password(passwordEncoder().encode("admin")).roles("USER", "ADMIN");
+
+        auth.userDetailsService( username -> {
+            final Member member = memberRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("There's no user: "+ username));
+            return User.builder()
+                    .username(member.getEmail())
+                    .password(member.getPassword())
+                    .roles("USER")
+                    .build();
+            });
     }
     /*
-        HTML 의 폼 태그를 넣어야함
+        각 필터에 대한 세부설정
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors() // spa 등 뭔가를 설정하는거 같음
-            .and()
+
+//                .cors() // spa 등 뭔가를 설정하는거 같음
+//            .and()
                 .csrf().disable() // 세션을 사용하지 않고 JWT 토큰을 활용하여 진행
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT 방식이므로 세션설정
-            .and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT 방식이므로 세션설정
+//            .and()
                 .authorizeRequests() // 인증절차에 대한 설정 진행
+                    .antMatchers("/login").permitAll()
                     .antMatchers("/api/**").authenticated()
+//                    .antMatchers("/api/**").permitAll()
                     .antMatchers("/test/**").permitAll()
+                .anyRequest().authenticated()
             .and()
                 .formLogin()
-//                .loginPage("/login") // 로그인 페이지 url
+//                .loginPage("/login") // 로그인 페이지 url (Default: springBoot 기본 제공)
+//                .loginProcessingUrl("/doLogin")
 //                .usernameParameter("username") // 지정 명칭
 //                .passwordParameter("password") // 지정 명칭
-//                .defaultSuccessUrl("/loging/result") // 로그인 성공 시 이동 페이지
+                .defaultSuccessUrl("/swagger-ui/index.html",false) // 로그인 성공 시 이동 페이지, 원래 가려던 페이지가 있는 경우 alwaysUse 옵션을 false로 설정해야됨
                 .permitAll()
             .and()
-                .logout();
+                .logout()
 //                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 지정 로그아웃 url
 //                .logoutSuccessUrl("/index") // log-out 성공 시 이동 페이지
 //                .invalidateHttpSession(true) // 세션 초기화
@@ -62,20 +81,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .exceptionHandling()
 //                .accessDeniedPage("/error"); // 에러 시 이동할 페이지
 //
-//                .and()
-//                .logout()
-//                .logoutUrl("/api/v1/users/sign-out");
+                .logoutUrl("/api/v1/users/sign-out");
     }
-//
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-//
-//    @Bean
-//    @SuppressWarnings("unused")
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    @SuppressWarnings("unused")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
